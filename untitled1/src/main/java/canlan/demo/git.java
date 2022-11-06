ecfb6c755d2f161b2c8a91cef4731d186d2b9732
canlan <1614501903@qq.com>
2022-11-06 18:56:54 +0800

    master commit2

--- untitled1/src/main/java/canlan/demo/git.java
+++ untitled1/src/main/java/canlan/demo/git.java
@@ -7,6 +7,6 @@ public class git {
         System.out.println("hello,git!");
         System.out.println("hello,git!11");
         System.out.println("hello,git!11");
-        System.out.println("hello,git!11");
+        System.out.println("hello,git!222222222222222");
     }
 }
