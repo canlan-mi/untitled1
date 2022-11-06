1e8231981d2a0e9792dd709131708e69ca6b9022
canlan <1614501903@qq.com>
2022-11-06 18:55:14 +0800

    hot-fix commit2

--- untitled1/src/main/java/canlan/demo/git.java
+++ untitled1/src/main/java/canlan/demo/git.java
@@ -7,6 +7,6 @@ public class git {
         System.out.println("hello,git!");
         System.out.println("hello,git!11");
         System.out.println("hello,git!11");
-        System.out.println("hello,git!11");
+        System.out.println("hello,git!111111111111111111111111");
     }
 }
